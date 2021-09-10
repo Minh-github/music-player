5a2a56de0718456779abeab978f129973da84f5c
Pham Quang Minh <zzphamminh219@gmail.com>
2021-09-10 14:58:24 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="style2.css">
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div class="warrper">
+    <div class="container">
+        <div class="image">
+            <img src="image__music1.jpg">
+        </div>
+        <div class="info">
+            <div class="title">Double Take</div>
+            <div class="description">Dhruv</div>
+        </div>
+        <div class="lyric">
+            I could say I never dare <br>
+            To think about you in that way, but<br>
+            I would be lyin'<br>
+            And I pretend I'm happy for you<br>
+            When you find some dude to take home<br>
+            But I won't deny that<br>
+            In the midst of the crowds<br>
+            In the shapes in the clouds<br>
+            I don't see nobody but you<br>
+            In my rose-tinted dreams<br>
+            Wrinkled silk on my sheets<br>
+            I don't see nobody but you<br>
+            Boy, you got me hooked onto something<br>
+            Who could say that they saw us coming?<br>
+            Tell me<br>
+            Do you feel the love?<br>
+            Spend a summer or a lifetime with me<br>
+            Let me take you to the place of your dreams<br>
+            Tell me<br>
+            Do you feel the love?<br>
+            And I could say I never unzipped<br>
+            Those blue Levi's inside my head<br>
+            But that's far from the truth<br>
+            Don't know what's come over me<br>
+            It seems like yesterday when I said<br>
+            "We'll be friends forever"<br>
+            Constellations of stars<br>
+            Murals on city walls<br>
+            I don't see nobody but you<br>
+            You're my vice, you're my muse<br>
+            You're a nineteenth floor view<br>
+            I don't see nobody but you<br>
+            Boy, you got me hooked onto something<br>
+            Who could say that they saw us coming?<br>
+            Tell me<br>
+            Do you feel the love?<br>
+            Spend a summer or a lifetime with me<br>
+            Let me take you to the place of your dreams<br>
+            Tell me<br>
+            Do you feel the love?<br>
+            Boy, you got me hooked onto something<br>
+            Who could say that they saw us coming?<br>
+            Tell me<br>
+            Do you feel the love?<br>
+            Spend a summer or a lifetime with me<br>
+            Let me take you to the place of your dreams<br>
+            Tell me<br>
+            Do you feel the love?<br>
+            Do you feel the love?<br>
+            Do you feel the love?<br>
+            Do you feel the love?<br>
+            Do you feel the love?<br>
+            Feel the love<br>
+            Do you feel the love?<br>
+        </div>
+        <audio id="audio__main" controls>
+            <source src="music1.mp3">
+        </audio>
+    </div>
+</div>
+</body>
+</html>
